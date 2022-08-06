b7b4eafee2c8064615bf45eba91ea3b66f59f15c
Andrey Komov <andrewkomov@gmail.com>
2022-08-06 20:15:08 +0300

    contacts-page

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const browsersync = require('browser-sync').create();
 
 // Sass task
 function scssTask(){
-    return src('scss/main.scss', { sourcemaps: true })
+    return src('scss/contacts.scss', { sourcemaps: true })
         .pipe(sass())
         .pipe(dest('css', { sourcemaps: '.'}));
 }
